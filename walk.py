3d0e7f26b233970f2d4baa8e4381c9c29b7369e1
Quyum Kehinde <quyumkehinde@gmail.com>
2023-04-27 23:34:46 +0100

    feat: solution

--- walk.py
+++ walk.py
@@ -1,8 +1,15 @@
 from os import listdir
 from os.path import isdir, join
 
+
 def walk_directory(d, indent):
     # Implement recursive directory walk here
+    for f in listdir(d):
+        print(indent, f)
+        f_path = join(d, f)
+        if isdir(f_path):
+            walk_directory(f_path, 2 * indent)
+
 
 # '.' means current directory
 walk_directory('Desktop', ' ')
